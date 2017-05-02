947bb24ebcc27938fb8fb52fbb61818e61da523a
Maksym Krutik <mkrutik@e1r5p12.unit.ua>
2017-05-02 17:22:18 +0300

    4

--- asm/main.c
+++ asm/main.c
@@ -1,34 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mkrutik <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/02 15:42:16 by mkrutik           #+#    #+#             */
-/*   Updated: 2017/05/02 16:14:32 by mkrutik          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "header.h"
-
-void	ft_open(char *name, t_file *src)
-{
-	src->fd = open(name);
-	(src->fd < 0) ? ft_error(3) : 0; 
-}
 
 void	ft_error(int n)
 {
-	ft_putstr("\033[0;31m");
-	ft_putstr("ERROR\n");
-	(n == 1) ? ft_putstr("Memory allocation error !\n") : 0;
-	(n == 2) ? ft_putstr("Wrong number of arguments!\n") : 0;
-    (n == 3) ? ft_putstr("File doesn`t open, maybe wrong file name\n") : 0;
-    (n == 4) ? ft_putstr("It`s not possible to read the file\n") : 0;
-    (n == 5) ? ft_putstr("It`s not possible to close the file\n") : 0;
-	ft_putstr("USAGE: ./lem-in <mapfile name>\n");
-	exit(-1);
+    ft_putstr("\033[0;31m");
+    ft_putstr("ERROR\n");
+    (n == 1) ? ft_putstr("Memory allocation error !\n") : 0;
+   	(n == 2) ? ft_putstr("Wrong number of arguments !\n") : 0;
+    (n == 3) ? ft_putstr("File doesn`t open, maybe wrong file name !\n") : 0;
+    (n == 4) ? ft_putstr("It`s not possible to read the file !\n") : 0;
+    (n == 5) ? ft_putstr("It`s not possible to close the file !\n") : 0;
+    ft_putstr("USAGE: ./lem-in <mapfile name>\n");
+    exit(-1);
+}
+
+void	ft_open(char *name, t_file *src)
+{
+	char s[2];
+
+	((src->fd = open(name, O_RDONLY)) < 0) ? ft_error(3) : 0;
+	(read(src->fd, s, 0) < 0) ? ft_error(4) : 0;
+	(close(src->fd) < 0) ? ft_error(5) : 0;
+	src->fd = open(name, O_RDONLY);
 }
 
 int		main(int argc, char **argv)
--- get_next_line.h
+++ get_next_line.h
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   header.h                                           :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkrutik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/02 15:42:55 by mkrutik           #+#    #+#             */
-/*   Updated: 2017/05/02 17:22:00 by mkrutik          ###   ########.fr       */
+/*   Created: 2017/01/06 12:15:34 by mkrutik           #+#    #+#             */
+/*   Updated: 2017/05/02 16:31:33 by mkrutik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HEADER_H
-# define HEADER_H
-# include "../libft/libft.h"
-# include "../get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 10000
+# include "libft/libft.h"
+# include <fcntl.h>
 
-typedef struct	s_gamer
+typedef struct		s_file
 {
-	char		*name;
-	char		*comment;
-	int			header;
-	int			fd;
-}				t_gamer;
+	int				fd;
+	char			*line;
+	struct s_file	*next;
+}					t_file;
 
-//void	ft_error(int n);
-
+int					get_next_line(const int fd, char **line);
 #endif
