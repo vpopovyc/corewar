a09d1246c7db3d776e2b8ba52d849c4011d65c6b
Maksym Krutik <mkrutik@e1r5p12.unit.ua>
2017-05-02 16:14:52 +0300

    2

--- asm/header.h
+++ asm/header.h
@@ -6,16 +6,20 @@
 /*   By: mkrutik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/02 15:42:55 by mkrutik           #+#    #+#             */
-/*   Updated: 2017/05/02 15:46:37 by mkrutik          ###   ########.fr       */
+/*   Updated: 2017/05/02 16:05:03 by mkrutik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
+# include "../libft/libft.h"
 
 typedef struct	s_file
 {
 	char		*name;
 	char		*comment;
 	int			header;
+	int			fd;
 }				t_file;
+
+#endif
