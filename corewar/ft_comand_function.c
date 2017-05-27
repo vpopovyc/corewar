967c497c797091a93b2c2c011d1cc2b47e9b5522
Maksym Krutik <mkrutik@e1r5p12.unit.ua>
2017-05-27 09:36:20 +0300

    lld

--- corewar/ft_comand_function.c
+++ corewar/ft_comand_function.c
@@ -63,6 +63,7 @@ void	ft_ld(t_corewar *data, t_carriage *src)
 	if (data->game_field[src->position + 1] == -48)
 	{
 		ft_take_arg(data, src, 0, data->game_field[src->position]);
+        (src->arg[1] > IDX_MOD) ? (src->arg[1] %= IDX_MOD) : 0;
 		if (src->arg[2] >= 0 && src->arg[2] <= REG_NUMBER)
 			src->reg[src->arg[2]] = (data->game_field[ft_if_negative(src->position - 5 + src->arg[1])] << 24) | (data->game_field[ft_if_negative(src->position - 5 + src->arg[1] + 1)] << 16) | (data->game_field[ft_if_negative(src->position - 5 + src->arg[1] + 2)] << 8) | data->game_field[ft_if_negative(src->position - 5 + src->arg[1] + 3)];
 	}
