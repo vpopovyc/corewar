f718da365021e374c44e32fadf06b5938a7d455a
Dmitry Kosolap <dkosolap@e1r4p11.unit.ua>
2017-06-01 13:41:09 +0300

    create carry.s

--- corewar/commands/ft_live.c
+++ corewar/commands/ft_live.c
@@ -4,11 +4,10 @@
 void    ft_live(t_corewar *data, t_carriage *src)
 {
     ft_inc_index(src);
-    src->arg[1] = (data->game_field[ft_inc_index(src)] << 24) | (data->game_field[ft_inc_index(src)] << 16) | (data->game_field[ft_inc_index(src)] << 8) | (data->game_field[ft_inc_index(src)]);
+    src->arg[1] = ((unsigned char)data->game_field[ft_inc_index(src)] << 24) | ((unsigned char)data->game_field[ft_inc_index(src)] << 16) | ((unsigned char)data->game_field[ft_inc_index(src)] << 8) | ((unsigned char)data->game_field[ft_inc_index(src)]);
     if (-src->arg[1] >= 1 && -src->arg[1] <= data->count_ply)
     {
         data->players_live[-src->arg[1] - 1]++;
-        src->live_in_cycle++;
        	data->winer = src->arg[1];
     }
     src->live_in_cycle++;
